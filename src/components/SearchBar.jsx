9e6f46d76e197c6921516144d2945e5364d768d5
Hilenn <hilenna@seznam.cz>
2020-11-06 13:14:22 +0100

    test

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function  SearchBar(props){
-
-
-return (
-     <div>
-         <input type="text" name=""/>
-     </div>
-
-)
-
-
+const SearchBar = (props) => {
     
-}
-export default SearchBar; 
+    const [detailFlight, setDetailFlight] = useState([])
+
+        useEffect(() => {
+            props.match.params && props.match.params.code && fetchingFlights()
+        }, [props.match.params.code])
+
+        const fetchingFlights = async () => {
+            const response = await fetch(api + props.match.params.code)
+            const data = await response.json()
+            setDetailFlight(data)
+            console.log(data)
+        };
+    }
+
+export default SearchBar;
+
